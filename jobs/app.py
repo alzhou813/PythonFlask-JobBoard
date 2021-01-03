c3d32090bb58abf3c36a5ce4fdaad81b42405418
Alvin Z <alzhou813@gmail.com>
2021-01-02 23:19:41 -0800

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -5,8 +5,9 @@ from flask import Flask, render_template, g
 PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
+
 def open_connection():
-	connection = getattr(g, '_connection', None)
+    connection = getattr(g, '_connection', None)
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
@@ -32,4 +33,5 @@ def close_connection(exception):
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-	return render_template("index.html")
+return render_template('index.html')
+    
