0748d19838262753682f7122874a8a1a7b7ade93
Alvin Z <alzhou813@gmail.com>
2021-01-03 00:17:50 -0800

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -35,3 +35,5 @@ def close_connection(exception):
 def jobs():
         jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
         return render_template('index.html', jobs=jobs)
+def job(job_id):
+         job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?', [job_id], single=True)
